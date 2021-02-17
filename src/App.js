4098f831e57269b814715a8dc930a2519c3f3c54
Sari Stissi <sestissi16@earlham.edu>
2021-02-16 18:45:25 -0800

    added new file and connected it to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Main from './Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>    
+            <Route exact path="/" component={Main} />
+            {/* <Route exact path="/TheGuardian" component={theGuardian} />
+            <Route exact path="/PlayTheGuardian" component={theGuardianPlay} />
+            <Route exact path="/SpaceSafe" component={spaceSafe} /> */}
+        </Router>
     </div>
   );
 }
