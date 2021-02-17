f88cf948066cf6d938a9dcf9d41d5e73c2d47031
Sari Stissi <sestissi16@earlham.edu>
2021-02-16 23:30:08 -0800

    fixed routing issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import React,  { Component } from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Main from './Main'
+import theGuardian from './Components/theGuardian'
+import theGuardianPlay from './Components/theGuardianPlay'
+import spaceSafe from './Components/spaceSafe'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <Router>    
-            <Route exact path="/" component={Main} />
-            {/* <Route exact path="/TheGuardian" component={theGuardian} />
-            <Route exact path="/PlayTheGuardian" component={theGuardianPlay} />
-            <Route exact path="/SpaceSafe" component={spaceSafe} /> */}
-        </Router>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Router>  
+                    <Switch>  
+                        <Route exact path="/" component={ Main } />
+                        <Route exact path="/TheGuardian" component={theGuardian} />
+                        <Route exact path="/PlayTheGuardian" component={theGuardianPlay} />
+                        <Route exact path="/SpaceSafe" component={spaceSafe} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
