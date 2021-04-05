bba409f7f3b3ad42b105fc80eebf3e64d62ac5d5
Sari Stissi <sestissi16@earlham.edu>
2021-04-04 19:06:14 -0700

    started adding elements to see if nav and styling were working correctly

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
+import portfolioInfo from '../portfolioData'
+import Tooltip from 'react-bootstrap'
 import './Header.css'
 
 class Header extends Component {
     render(){
-        return (
-            <section id="Header">
-                <div id="headerBanner">
+        const { height } = this.props
+        const {
+            briefDescrip,
+            city,
+            currentOccupation,
+            social
+        } = portfolioInfo.main
 
+        var networks= function(social) {
+            for(const item in social){
+                return <li key={item.name}><a href={item.url}><i className={item.className}> {item.name}</i></a></li>
+            }
+        }
+
+        return (
+            <section id="Header" style={{ height: height }}>
+                <div id="headerBanner">
+                    <h1 id="headerBannerTitle">I'm Sari Stissi</h1>
+                    <h2>{briefDescrip}</h2>
+                    <h3>I'm currently working as <span>{currentOccupation}</span> based in {city}</h3>
+                    <ul className="social">
+                        {networks}
+                    </ul>
                 </div>
             </section>
         );
