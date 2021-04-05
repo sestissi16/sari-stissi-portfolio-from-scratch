2e959d823844cfbebffae8242dfdc8c6f4f1586a
Sari Stissi <sestissi16@earlham.edu>
2021-04-04 19:09:33 -0700

    fixed smooth scroll issues

--- src/Sections/NavBarComp.js
+++ src/Sections/NavBarComp.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { animateScroll as scroll, scroller } from "react-scroll";
 import './NavbarComp.css'
 
 class NavbarComp extends Component {
+
     render(){
         return (
             <Navbar id="navbarContainer" fixed="top" className="custom-navbar-bg">
-                <Navbar.Brand className="smoothscroll" href="#home"><h1 id="navBarBrand">Sari Stissi</h1></Navbar.Brand>
+                <Navbar.Brand 
+                    onClick={() => scroller.scrollTo('Header', {
+                        spy: true,
+                        smooth: true,
+                        offset: -70,
+                        duration: 500,
+                    })}
+                    href="/"
+                >
+                    <h1 id="navBarBrand">Sari Stissi</h1>
+                </Navbar.Brand>
                 <Nav className="navBarLinkCont">
-                    <Nav.Link className="navBarLink smoothscroll" href="#About">About</Nav.Link>
-                    <Nav.Link className="navBarLink smoothscroll" href="#Resume">Resume</Nav.Link>
-                    <Nav.Link className="navBarLink smoothscroll" href="#Portfolio">Portfolio</Nav.Link>
-                    <Nav.Link className="navBarLink smoothscroll" href="#Contact">Contact</Nav.Link>
+                    <Nav.Link 
+                        className="navBarLink"
+                        onClick={() => scroller.scrollTo('About', {
+                            spy: true,
+                            smooth: true,
+                            offset: -70,
+                            duration: 500,
+                        })}
+                        href="#About"
+                    >
+                        About
+                    </Nav.Link>
+                    <Nav.Link 
+                        className="navBarLink"
+                        onClick={() => scroller.scrollTo('Resume', {
+                            smooth: true,
+                            offset: -70,
+                            duration: 500,
+                        })}
+                        href="#Resume"
+                    >
+                        Resume
+                    </Nav.Link>
+                    <Nav.Link 
+                        className="navBarLink"
+                        onClick={() => scroller.scrollTo('Portfolio', {
+                            smooth: true,
+                            offset: -70,
+                            duration: 500,
+                        })}
+                        href="#Portfolio"
+                    >
+                        Portfolio
+                    </Nav.Link>
+                    <Nav.Link 
+                        className="navBarLink"
+                        onClick={() => scroller.scrollTo('Contact', {
+                            smooth: true,
+                            offset: -70,
+                            duration: 500,
+                        })}
+                        href="#Contact"
+                    >
+                        Contact
+                    </Nav.Link>
                 </Nav>
             </Navbar>
         );
