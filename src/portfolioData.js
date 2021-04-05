d34d7f15a96d181e60fbc7c0d99a559fcaf429ed
Sari Stissi <sestissi16@earlham.edu>
2021-04-04 19:08:24 -0700

    file to house info that could change for easy updating

---
+++ src/portfolioData.js
@@ -0,0 +1,39 @@
+// Object with data that might change for easier updating
+const portfolioInfo = {
+    main: {
+        briefDescrip: "A multifaceted learner looking actively help the lives of others",
+        bio1: "",
+        bio2: "",
+        bio3: "",
+        profilePic: "",
+        city: "Las Vegas",
+        state: "NV",
+        currentOccupation: "Junior Developer",
+        phone: "(702) 738-4277",
+        email: "s3937stissi@gmail.com",
+        social: {
+            facebook: {
+                name:"Facebook",
+                url:"https://www.facebook.com/Sari.Stissi",
+                className:"fa fa-facebook"
+            },
+            linkedin: {
+                name:"Linkedin",
+                url:"https://www.linkedin.com/in/sari-stissi/",
+                className:"fa fa-linkedin"
+            },
+            instagram: {
+                name:"Instagram",
+                url:"https://www.instagram.com/sarinotsorry00/",
+                className:"fa fa-instagram"
+            },
+            github: {
+                name:"GitHub",
+                url:"https://github.com/sestissi16",
+                className:"fa fa-github"
+            }
+        }
+    }
+}
+
+export default portfolioInfo
